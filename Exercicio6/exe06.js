01bc279ea7433f01f9dca5a48994c74831a240b1
Tamashiro90 <leandro.tamashiro90@gmail.com>
2024-09-26 21:20:22 -0300

    Add files via upload

---
+++ Exercicio6/exe06.js
@@ -0,0 +1,8 @@
+var input1=parseFloat(prompt('Inserir um número'))
+var input2=parseFloat(prompt('Inserir um número'))
+var input3=parseFloat(prompt('Inserir um número'))
+
+var dados=[input1, input2, input3];
+
+console.log(`${dados[0]}, ${dados[1]}, ${dados[2]},`)
+document.write(`\n${dados[2]}, ${dados[1]}, ${dados[0]}.`)
